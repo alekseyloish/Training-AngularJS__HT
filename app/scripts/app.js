0c5a47d0350f47df53547ec1491494d4ae13aa8e
Aleksey Loish <aleksey.loish@gmail.com>
2016-02-25 21:12:14 +0200

    install yeoman tool

---
+++ app/scripts/app.js
@@ -0,0 +1,6 @@
+// Follow steps from the walkthrough guide to create HomeController
+(function () {
+    'use strict';
+
+    angular.module('auction', []);
+}());
--- scripts/app.js
+++
@@ -1,6 +0,0 @@
-// Follow steps from the walkthrough guide to create HomeController
-(function () {
-    'use_strict';
-
-    angular.module('auction', []);
-}());
